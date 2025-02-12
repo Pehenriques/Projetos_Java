b046bf400abbaac5487527f452412dec9c26937f
Pedro henrique <ph2696161@gmail.com>
2025-02-11 22:05:08 -0300

    ajuste e Operadores

---
+++ BootCamp_Bradesco_Java/Tipos-variaveis/src/Operadores.java
@@ -0,0 +1,59 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+
+        // Operadores Aritméticos
+        int a = 10;
+        int b = 2;
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplicacao = a * b;
+        int divisao = a / b;
+        int modulo = a % b;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisão: " + divisao);
+        System.out.println("Módulo: " + modulo);
+
+        // Operadores de Comparação
+        boolean maior = a > b;
+        boolean menor = a < b;
+        boolean igual = a == b;
+        boolean diferente = a != b;
+        boolean maiorIgual = a >= b;
+        boolean menorIgual = a <= b;
+
+        System.out.println("Maior: " + maior);
+        System.out.println("Menor: " + menor);
+        System.out.println("Igual: " + igual);
+        System.out.println("Diferente: " + diferente);
+        System.out.println("Maior ou Igual: " + maiorIgual);
+        System.out.println("Menor ou Igual: " + menorIgual);
+
+        // Operadores Lógicos
+        boolean e = (a > b) && (a < b);
+        boolean ou = (a > b) || (a < b);
+        boolean negacao = !(a > b);
+
+        System.out.println("E: " + e);
+        System.out.println("Ou: " + ou);
+        System.out.println("Negação: " + negacao);
+
+        // Operadores de Atribuição
+        int c = 10;
+        c += 2; // c = c + 2
+        c -= 2; // c = c - 2
+        c *= 2; // c = c * 2
+        c /= 2; // c = c / 2
+        c %= 2; // c = c % 2
+
+        System.out.println("Atribuição: " + c);
+
+        // Operadores Unários
+        int d = 10;
+        d++; // d = d + 1
+        d--; //d = d - 1
+
+    }
+}
