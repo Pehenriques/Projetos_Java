c6b502aaecaaa106e454f020f3fb34d13337524c
Pedro henrique <ph2696161@gmail.com>
2025-02-11 21:45:35 -0300

    adiciona estrutura inicial do projeto Java e documentação

---
+++ Tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,23 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+
+        String meuNome = "Pedro";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto= 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double PI = 3.14;
+
+
+        //System.out.println("Hello, World!");
+    }
+}
