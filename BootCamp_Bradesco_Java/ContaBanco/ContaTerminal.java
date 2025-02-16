a69a661f239a73fecb97ad74a4de9ee5573c8565
Pedro henrique <ph2696161@gmail.com>
2025-02-16 13:24:12 -0300

    Deafio

---
+++ BootCamp_Bradesco_Java/ContaBanco/ContaTerminal.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+    
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("Digite o número da conta: ");
+        Scanner sc = new Scanner(System.in);    
+        numeroConta = sc.nextInt();
+    
+        System.out.println("Digite a agência: ");
+        sc.nextLine();
+        agencia = sc.nextLine();
+
+        System.out.println("Digite o nome do titular da conta: ");
+        nomeCliente = sc.nextLine();
+        
+        System.out.println("Digite o saldo da conta: ");
+        saldo = sc.nextDouble();    
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência e " + agencia + ", conta " + numeroConta+ " e seu saldo R$ " + saldo +" ja está disponível para saque");
+    
+
+        
+    }
+}
